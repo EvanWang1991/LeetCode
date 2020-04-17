e9d32969503b3b18ceb71f1613f70b904eaa7513
JiXiang Wang <jixiang.wang@openjawtech.com>
2020-04-17 11:57:57 +0800

    add SumNums.java

---
+++ src/main/java/com/wing/leetcode/medium/SumNums.java
@@ -0,0 +1,34 @@
+package com.wing.leetcode.medium;
+
+/**
+ *
+ * 求 1+2+...+n ，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。
+ *
+ * 示例 1：
+ *
+ * 输入: n = 3
+ * 输出: 6
+ * 示例 2：
+ *
+ * 输入: n = 9
+ * 输出: 45
+ *  
+ *
+ * 限制：
+ *
+ * 1 <= n <= 10000
+ *
+ */
+public class SumNums {
+
+    public static void main(String[] args) {
+        System.out.println(solution(9));
+    }
+    public static int solution(int n) {
+        if (n == 1 || n == 0){
+            return n;
+        }else {
+            return n + solution(n -1);
+        }
+    }
+}
